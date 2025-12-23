49781198d83827a910801864f3c3408c22744ce3
Jacqueline Guo <guojacqu@usc.edu>
2025-12-22 16:23:24 -0800

    pushing final changes to project 1!

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,9 @@ export default function Main() {
   return (
     <main>
       <h1>Why I'm excited to learn React:</h1>
-      <ol>
+      <ol className="ordered-list">
         <li className="list-item">I want to become a design engineer who understands the underlying technologies of what I'm building.</li>
-        <li className="list-item">I want to build a really beautiful personal website.</li>
+        <li className="list-item">I want to build a really beautiful personal website with my work and thoughts.</li>
       </ol>
     </main>
   )
